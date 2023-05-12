811fdc5c501cf0f424ee23112f7811cfeb930b87
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2023-05-12 17:53:08 -0500

    added user intake slash command

--- commands/userIntake.js
+++ commands/userIntake.js
@@ -1,10 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { initUser } = require('../utils/mongoose/dbOperations');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Saves your LeetCode username'),
+		.setDescription('Saves your LeetCode username')
+        .addStringOption(option =>
+            option.setName('username')
+                .setDescription('Your leetCode username')
+                .setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Response appeared');
+        const usertag = interaction.user.tag;
+        const username = interaction.options.getString('username');
+        try{
+            replyStr = await initUser(usertag, username);
+            await interaction.reply(replyStr);
+        }
+        catch(e){
+            await interaction.reply(`Username could not be saved: ${e}`);
+        }
+		
 	},
 };
