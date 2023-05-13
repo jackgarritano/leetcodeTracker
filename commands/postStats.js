3428cc954f7234c0c18507d3ac3e78c91c7f9d7e
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2023-05-13 11:49:47 -0500

    profile command can now get other people's profiles

--- commands/postStats.js
+++ commands/postStats.js
@@ -5,11 +5,20 @@ const { generateUserProfile } = require('../utils/leetCode/userData');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile')
-        .setDescription('Shows your updated leetcode profile'),
+        .setDescription('Shows updated leetcode profile')
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('optionally @ a user')),
     async execute(interaction){
-        const usertag = interaction.user.tag;
+        let targetUser;
+            if(interaction.options.getUser('target')){
+                targetUser = interaction.options.getUser('target').tag;
+            }
+            else{
+                targetUser = interaction.user.tag;
+            }
         try{
-            const username = await checkUserExistence(usertag);
+            const username = await checkUserExistence(targetUser);
             const pngBuffer = await generateUserProfile(username);
             const attachment = new AttachmentBuilder(pngBuffer, {name: 'profile.png'});
             await interaction.reply({files: [attachment]});
